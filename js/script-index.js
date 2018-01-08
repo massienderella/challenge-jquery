d84fed37848e743823ad1f421ab4ab7c93d292b0
Massienderella <massiel.ams@gmail.com>
2018-01-08 18:38:35 -0300

    Etapa 5: Está buena pero no funciona :(

--- js/script-index.js
+++ js/script-index.js
@@ -6,13 +6,10 @@ $(document).ready( function(){
 
 //La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 renderHighlightedRecipes(recipesArray);
-
+renderActivities(activitiesArray);
 });
 
-/*
-* Función que se encarga de pintar TODAS las recetas que tengan 
-* marcado el atributo "highlighted" como TRUE
-*/
+//Función que se encarga de pintar TODAS las recetas que tengan marcado el atributo "highlighted" como TRUE//
 function renderHighlightedRecipes(recipesArray) {
 
 	for(i = 0 ; i <= recipesArray.length ; i++){
@@ -21,6 +18,20 @@ function renderHighlightedRecipes(recipesArray) {
 		}
 	}
 	console.log('Recipes: ', recipesArray);
+}
+
+//Función que se encarga de pintar todas las actividades//
+
+function renderActivities(activitiesArray) {
+    console.log('Activities: ', activitiesArray);
+
+for ( var i = 0; i < activitiesArray.length; i++){
+    renderActivity(activitiesArray[i]);
+    }
+     if (activitiesArray.length > 0){
+         $('.wrapper-message').hide();
+     }
+    
 }
 
 /*
@@ -45,14 +56,6 @@ function renderRecipe(recipe) {
 
 	console.log('Voy a pintar la receta: ', recipe);
 }
-
-/*
-* Función que se encarga de pintar todas las actividades
-*/
-function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
-}
-
 /*
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el 
